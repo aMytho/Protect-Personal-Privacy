70efbc51e02987b0d701f04f6d0ed547b3b0cd4c
MythoAway7 <58316242+MythoAway7@users.noreply.github.com>
2023-03-21 17:23:13 -0500

    Add browser guide

---
+++ src/app/guide/browser/browser.component.ts
@@ -0,0 +1,24 @@
+import { Component, ElementRef, ViewChild } from '@angular/core';
+import { browserSelection } from './browser.types';
+
+@Component({
+  selector: 'app-browser',
+  templateUrl: './browser.component.html'
+})
+export class BrowserComponent {
+  @ViewChild("chrome")
+  chrome!: ElementRef;
+  @ViewChild("firefox")
+  firefox!: ElementRef;
+  
+  public browser: browserSelection = "";
+
+  public swapBrowser(browser: browserSelection) {
+    this.browser = browser;
+    if (browser == "Chrome") {
+      this.chrome.nativeElement.scrollIntoView({ behavior: 'smooth', block: 'start' });
+    } else {
+      this.firefox.nativeElement.scrollIntoView({ behavior: 'smooth', block: 'start' });
+    }
+  }
+}

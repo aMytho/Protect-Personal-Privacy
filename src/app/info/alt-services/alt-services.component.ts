abe13bdb08e43d65e727527ef3f4feb43359b0f7
MythoAway7 <58316242+MythoAway7@users.noreply.github.com>
2023-03-21 20:04:15 -0500

    Add scrolling to alt services

--- src/app/info/alt-services/alt-services.component.ts
+++ src/app/info/alt-services/alt-services.component.ts
@@ -1,9 +1,16 @@
-import { Component, OnInit, ViewChild } from '@angular/core';
+import { Component} from '@angular/core';
 @Component({
   selector: 'app-alt-services',
   templateUrl: './alt-services.component.html',
   styleUrls: ['./alt-services.component.css']
 })
 export class AltServicesComponent {
-  
+  public scrollTo(element: string) {
+    // Technically this is the non angular way, but it works :)
+    let target = document.getElementById(element)!;
+    window.scroll({
+      top: target.getBoundingClientRect().top + window.scrollY,
+      behavior: 'smooth',
+    });
+  }
 }

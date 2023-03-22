f0d2cbadcb574dbb7a77a877fc5930ef4613fb95
MythoAway7 <58316242+MythoAway7@users.noreply.github.com>
2023-03-21 20:46:23 -0500

    Start user info page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AltServicesComponent } from './info/alt-services/alt-services.component';
+import { InfoComponent } from './info/info.component';
 
 const routes: Routes = [
   {path: "cases", loadChildren: () => import('./case/case.module').then(m => m.CaseModule)},
   {path: "guide", loadChildren: () => import("./guide/guide.module").then(m => m.GuideModule)},
+  {path: "myself", component: InfoComponent},
   {path: "alternatives", component: AltServicesComponent}
 ];
 
--- src/app/info/info.component.ts
+++ src/app/info/info.component.ts
@@ -1,10 +1,52 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-info',
   templateUrl: './info.component.html',
   styleUrls: ['./info.component.css']
 })
-export class InfoComponent {
+export class InfoComponent implements OnInit {
+  public nav = window.navigator;
 
+  public info = {
+    timeOpened:new Date(),
+    timezone:(new Date()).getTimezoneOffset()/60,
+    pageon: window.location.pathname,
+    referrer: document.referrer,
+    previousSites: history.length,
+    browserName: navigator.appName,
+    browserEngine: navigator.product,
+    browserVersion1a: navigator.appVersion,
+    browserVersion1b: navigator.userAgent,
+    browserLanguage: navigator.language,
+    browserOnline: navigator.onLine,
+    browserPlatform: navigator.platform,
+    javaEnabled: navigator.javaEnabled(),
+    dataCookiesEnabled: navigator.cookieEnabled,
+    dataCookies1: document.cookie,
+    dataCookies2: decodeURIComponent(document.cookie.split(";").toString()),
+    dataStorage: null,
+    sizeScreenW: screen.width,
+    sizeScreenH: screen.height,
+    sizeInW: innerWidth,
+    sizeInH: innerHeight,
+    sizeAvailW: screen.availWidth,
+    sizeAvailH: screen.availHeight,
+    scrColorDepth: screen.colorDepth,
+    scrPixelDepth: screen.pixelDepth,
+    // latitude(){return position.coords.latitude},
+    // longitude(){return position.coords.longitude},
+    // accuracy(){return position.coords.accuracy},
+    // altitude(){return position.coords.altitude},
+    // altitudeAccuracy(){return position.coords.altitudeAccuracy},
+    // heading(){return position.coords.heading},
+    // speed(){return position.coords.speed},
+    // timestamp(){return position.timestamp},
+
+  };
+
+  ngOnInit(): void {
+
+    
+  }
 }

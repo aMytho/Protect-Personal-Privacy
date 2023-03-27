ab02f1069037c1d972671f848662abb1e7eb4869
MythoAway7 <58316242+MythoAway7@users.noreply.github.com>
2023-03-26 20:33:30 -0500

    Add new color scheme

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,12 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        'lapis': "#176087"
+        'lapis': "#176087",
+        'metal': "#122C34",
+        'indigo': "#224870",
+        'martianBlue': "#2A4494",
+        "pictonBlue": "#4EA5D9",
+        "eggBlue": "#44CFCB"
       },
     },
   },

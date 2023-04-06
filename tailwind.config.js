ba027610e0d5a45b4fbec936bf59809f0b6f2b3b
MythoAway7 <58316242+MythoAway7@users.noreply.github.com>
2023-04-05 20:08:19 -0500

    Last design overhaul (probably)

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,7 +11,10 @@ module.exports = {
         'indigo': "#224870",
         'martianBlue': "#2A4494",
         "pictonBlue": "#4EA5D9",
-        "eggBlue": "#44CFCB"
+        "eggBlue": "#44CFCB",
+        "lighterBlue": "#3180ab",
+        "buttonNormal": "#80b0dd4c",
+        "buttonHover": "rgba(128, 176, 221, 0.64)"
       },
     },
   },

645746b4293f1241fc8a0414e25d69f42e9228aa
MythoAway7 <58316242+MythoAway7@users.noreply.github.com>
2023-04-15 17:53:58 -0500

    Test of server

---
+++ server.js
@@ -0,0 +1,19 @@
+// Runs the express server to serve angular app
+
+// Use Express
+var express = require("express");
+
+// Create new instance of the express server
+var app = express();
+
+// Create link to Angular build directory
+// The `ng build` command will save the result
+// under the `dist` folder.
+var distDir = __dirname + "/dist/";
+app.use(express.static(distDir));
+
+// Init the server
+var server = app.listen(process.env.PORT || 8080, function () {
+    var port = server.address().port;
+    console.log("App now running on port", port);
+});
